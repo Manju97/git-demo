ec453f890cbe00595648250b1d13081793e2c4c0
Manju97 <44949304+Manju97@users.noreply.github.com>
2021-03-04 11:33:31 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,17 @@
+# Add implementation
+def add(x,y):
+	pass
+
+# Subtract implementation
+def subtract(x,y):
+    pass
+
+# Multiply implementation
+def multiply(x,y):
+	return x*y
+
+# Divide implementation
+def divide(x,y):
+if y==0
+  divide by 0
+
